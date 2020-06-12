95733f58ee35331d6e3748d21245546c36b19509
gaserashraf <elkinggaser@gmail.com>
2020-06-12 15:30:26 +0200

    final

--- javascript.js
+++
@@ -1,8 +0,0 @@
-
-var text=document.getElementById('t')
-text.onkeyup=function()
-{
-   var textArea=document.getElementById('t').value;
-   document.getElementById('f').srcdoc=textArea;
-}
-console.log('dadas');
